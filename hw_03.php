2124d1753059e3615eef2f2e10398d269f072370
Roman Mamedov <68127329+Mamedzuka@users.noreply.github.com>
2022-03-18 22:00:32 +0300

    Update hw_03.php

--- hw_03.php
+++ hw_03.php
@@ -1,10 +1,43 @@
+interface Human {
+    public function IDontUnderstand();
+    public function WheresTheTruth();
+}
+
 class User {
     private const CITY = "Jaroslavl";
     public const AGE = 18;
-    public static int $cash = "300$";
+    public static int $cash = 300;
     public string $name = "Aboba";
-    function __construct(string $in_name, string $in_cash) {
-        self->name = $in_name;
+    function __construct(string $in_name, int $in_cash) {
+        $this->$name = $in_name;
         self::$cash = $in_cash;
+        echo "Конструктор"
+    }
+    function __destruct() {
+        echo "Уничтожается " . __CLASS__ ;
+    }
+    public function IDontUnderstand() {
+        return "What's going on here?"
+    }
+    public function WheresTheTruth() {
+        string $quote = "Your're " . (string)AGE . " years old?";
+        return $quote;
+    }
+}
+
+abstract class Client {
+    abstract public function WhatsUp(string $emotion);
+    private static function ImOk() {
+        return "I'm okey, dude!";
+    }
+}
+
+class AngryClient extends Client {
+    public function WhatsUp(string $emotion) {
+        if ($emotion == "anger") {
+            echo "Dude, wtf, i need my money back!";
+        } else {
+            echo self::ImOk();
+        }
     }
 }
